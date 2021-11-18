43199cd2784233be24409b7ece235dcab9897133
tonybrand633 <ericenv@aliyun.com>
2021-11-19 01:20:53 +0800

    Test

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    [SerializeField]
+    private WeaponType _type;
+
     // Start is called before the first frame update
     void Start()
     {
